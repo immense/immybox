a368608bd3b62297d943cc44dc54047d7eeb9481
Christian Michael Paul Bankester <cbankester@immense.net>
2016-08-26 14:53:47 -0500

    Added the rest of the shell script

--- make-gh-pages-content.sh
+++ make-gh-pages-content.sh
@@ -14,20 +14,20 @@ fi
 
 git checkout gh-pages
 git reset --hard "$1"
-# rm -rf node_modules
-# npm install
-# npm run build
-#
-# # remove everything but the build, demo and .git directories
-# for file in `ls -a | awk '!/^..?$|build|demo|^.git$/'`; do
-#   rm -rf "$file"
-# done
-#
-# # create CNAME file
-# printf "immybox.js.org" > CNAME
-#
-# # flatten demo dir
-# mv demo/* .
-#
-# # remove index.js (we only need index-es5.js)
-# rm -rf index.js
+rm -rf node_modules
+npm install
+npm run build
+
+# remove everything but the build, demo and .git directories
+for file in `ls -a | awk '!/^..?$|build|demo|^.git$/'`; do
+  rm -rf "$file"
+done
+
+# create CNAME file
+printf "immybox.js.org" > CNAME
+
+# flatten demo dir
+mv demo/* .
+
+# remove index.js (we only need index-es5.js)
+rm -rf index.js
